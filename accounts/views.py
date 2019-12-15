f4362219bd1114ff0ea0cd3e455cfaae03cbd70a
Apekshit <apekshit33@gmail.com>
2019-12-15 21:10:18 +0530

    Setup signup and login pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def signup(request):
+	return render(request, 'accounts/signup.html')
+
+def login(request):
+	return render(request, 'accounts/login.html')
+
+def logout(request):
+	# TODO Need to rout to homepage
+	return render(request, 'accounts/signup.html')
